fa860c8e1fdd4f8ce861c4b54c20fa4c1e3d73fa
SasEdina <139265679+SasEdina@users.noreply.github.com>
2024-04-09 20:57:05 +0300

    Am adaugat modala de subscribe

--- Proiect final/index.js
+++ Proiect final/index.js
@@ -37,6 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     initShopCount();
+    initModal();
 });
 
 //Lista de cumparaturi
@@ -58,4 +59,31 @@ function addToCart(name, price) {
 
     const addToCartButtons = document.getElementById("shop-cart-count");
     addToCartButtons.innerHTML= shopList.length;
+}
+
+function initModal() {
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("subscribeBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
